3f17a7ca9ccad0b6ae781b29a76b6aac32437530
Rob King <rob@gcsuk.co.uk>
2015-09-12 16:40:06 +0100

    Refactored Git Javascript

---
+++ LCARS/Scripts/git.js
@@ -0,0 +1,10 @@
+﻿function apiStatus(data) {
+
+    if (data.status === "good") {
+        window.location = "/";
+    } else {
+        $("#gitStatus").text("GitHub Status: " + data.status);
+        $("#gitStatusMessage").text(data.body);
+        $("#gitStatusDate").text(moment(data.created_on).format("DD MMMM YYYY hh:mm"));
+    }
+}
