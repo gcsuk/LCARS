0c44e9a6dffd8caf5a4898e019a0c1f4d9836e62
Rob King <rob.king@bedegaming.com>
2015-12-11 10:16:12 +0000

    Amended GetScreen javascript call in Admin area

--- LCARS/Controllers/AdminController.cs
+++ LCARS/Controllers/AdminController.cs
@@ -84,15 +84,12 @@ namespace LCARS.Controllers
         }
 
         [Route("Admin/GetScreen/{id}")]
-        public JsonResult GetScreen(int id = 0)
+        public JsonResult GetScreen(int id)
         {
             switch (id)
             {
                 case -1:
                     return Json(new ViewModels.Screens.Screen(), JsonRequestBehavior.AllowGet);
-                case 0:
-                    return Json(_screensDomain.GetScreens(Server.MapPath(@"~/App_Data/Screens.json")).First(),
-                        JsonRequestBehavior.AllowGet);
                 default:
                     return Json(_screensDomain.GetScreens(Server.MapPath(@"~/App_Data/Screens.json")).Single(i => i.Id == id),
                         JsonRequestBehavior.AllowGet);
