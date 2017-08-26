f502681b734a9f5ad19fafa051569674ec8a7ad6
Rob King <rob@gcsuk.co.uk>
2017-08-26 23:39:21 +0100

    Fixed DeploymentsPage to use a table for layout

--- LCARS/Client/js/components/DeploymentsPage.js
+++ LCARS/Client/js/components/DeploymentsPage.js
@@ -2,21 +2,24 @@ import React, {Component} from 'react';
 import {Link} from 'react-router';
 import styled from 'styled-components';
 
-const Deployments = styled.div`
-  display: grid;
-  grid-template-rows: repeat(${props => (props.rowCount)}, 100px);
-  grid-template-columns: repeat(${props => (props.colCount)}, 100px); 
+const Deployments = styled.table`
+  width: 100%;
+  text-align: center;
+  vertical-align: middle
 `;
 
-const ProjectName = styled.div`
-  grid-row: ${props => (props.rowIndex + 1)} / ${props => (props.rowIndex + 2)};
-  border: solid 1px #fff
+const ProjectName = styled.td`
+  width: 150px;
+  text-align: left
 `;
 
 const ProjectDeploy = styled.div`
-  grid-row: ${props => (props.rowIndex + 1)} / ${props => (props.rowIndex + 2)};
-  grid-column: ${props => (props.colIndex + 2)} / ${props => (props.colIndex + 3)};
-  border: solid 1px #fff
+  display: block;
+  height: 40px;
+  margin: 5px;
+  font-size: 30px;
+  border-radius: 20px;
+  background-color: #015001
 `;
 
 class DeploymentsPage extends Component { 
@@ -27,10 +30,13 @@ class DeploymentsPage extends Component {
 
     for (var row = 0; row < 5; row++){
         deployments[row] = {};
-        deployments[row].name = "Zeus " + row;
+        deployments[row].name = "Project " + row;
         deployments[row].deploys = new Array(5);
         for(var col = 0; col < 5; col++){ 
-            deployments[row].deploys[col] = row + '.' + col;
+            deployments[row].deploys[col] = {};
+            deployments[row].deploys[col].env = "ENV 01";
+            deployments[row].deploys[col].status = "OK";
+            deployments[row].deploys[col].version = "1.0.1.2"
         }
     }
 
@@ -39,19 +45,29 @@ class DeploymentsPage extends Component {
 
   render() {
     return (
-      <Deployments rowCount={this.state.deployments.length} colCount={this.state.deployments[0].deploys.length}>
-        { this.state.deployments.map((project, rowIndex) => (
-          <div key={rowIndex} rowIndex={rowIndex}>
-            <ProjectName rowIndex={rowIndex}>
-              {project.name}
-            </ProjectName>
-            { project.deploys.map((item, colIndex) => (
-              <ProjectDeploy key={colIndex} rowIndex={rowIndex} colIndex={colIndex} className="project-deploy">
-                {item}
-              </ProjectDeploy>
-            ))}
-          </div>)
-        )}
+      <Deployments>
+        <thead>
+          <th></th>
+          { this.state.deployments[0].deploys.map((item, colIndex) => (
+            <th key={colIndex}>
+              {item.env}
+            </th>
+          ))}
+        </thead>
+        <tbody>
+          { this.state.deployments.map((project, rowIndex) => (
+            <tr key={rowIndex}>
+              <ProjectName>
+                {project.name}
+              </ProjectName>
+              { project.deploys.map((item, colIndex) => (
+                <td key={colIndex}>
+                  <ProjectDeploy>{item.version}</ProjectDeploy>
+                </td>
+              ))}
+            </tr>)
+          )}
+        </tbody>
       </Deployments>
     );
   }
