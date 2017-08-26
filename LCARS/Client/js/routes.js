facddd9746d7a652c8faefe9b626ac9fa18945bd
Rob King <rob@gcsuk.co.uk>
2017-08-27 00:20:17 +0100

    Added EnvironmentsPage

--- LCARS/Client/js/routes.js
+++ LCARS/Client/js/routes.js
@@ -10,7 +10,7 @@ import IssuesPage from './components/IssuesPage';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={DeploymentsPage} />
+    <IndexRoute component={EnvironmentsPage} />
     <Route path="builds" component={BuildsPage} />
     <Route path="deployments" component={DeploymentsPage} />
     <Route path="git" component={GitPage} />
