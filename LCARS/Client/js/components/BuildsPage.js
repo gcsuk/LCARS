d86258907701ebd4327ebad61b94faf5e5f18297
Rob King <rob@gcsuk.co.uk>
2017-08-27 17:57:41 +0100

    Added BuildsPage

--- LCARS/Client/js/components/BuildsPage.js
+++ LCARS/Client/js/components/BuildsPage.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router';
+import styled from 'styled-components';
 
-class BuildsPage extends React.Component {
+const Builds = styled.div`
+  text-align: center;
+`;
+
+const Build = styled.div`
+  margin: 10px 0
+`;
+
+const ProjectName = styled.span`
+  display: inline-block;
+  width: 200px;
+  text-align: left
+`;
+
+const BuildStatus = styled.span`
+  display: inline-block;
+  padding: 3px 35px;
+  font-size: 30px;
+  border-radius: 20px;
+  background-color: #015001
+`;
+
+class BuildsPage extends Component { 
+  constructor(props) {
+    super(props);
+    
+    const builds = new Array(5);
+
+    for (var row = 0; row < 5; row++){
+        builds[row] = {};
+        builds[row].name = "Project " + row;
+        builds[row].status = "SUCCESS";
+        builds[row].version = row + ".0.0.0";
+    }
+
+    this.state = {builds: builds};
+  }
+
   render() {
     return (
-      <div>
-        <h1>Builds</h1>
-      </div>
+      <Builds>
+        { this.state.builds.map((project, rowIndex) => (
+          <Build>
+            <ProjectName row={rowIndex}>
+              {project.name}
+            </ProjectName>
+            <BuildStatus row={rowIndex}>
+              {project.status} - {project.version}
+            </BuildStatus>
+          </Build>)
+        )}
+      </Builds>
     );
   }
 }
