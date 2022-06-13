d9fdf279f4eb5af3cb57aa49003fc5eef43970e2
Rob King <rob@gcsuk.co.uk>
2022-06-13 22:54:05 +0100

    Added ping endpoint

---
+++ Server/LCARS/LCARS/Endpoints/PingEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using LCARS.Endpoints.Internal;
+
+namespace LCARS.Endpoints;
+
+public class PingEndpoints : IEndpoints
+{
+    private const string Tag = "Ping";
+    private const string BaseRoute = "ping";
+
+    public static void DefineEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet($"{BaseRoute}", () => Results.NoContent())
+            .WithName("Ping")
+            .Produces(204)
+            .WithTags(Tag);
+    }
+
+    public static void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+    }
+}
