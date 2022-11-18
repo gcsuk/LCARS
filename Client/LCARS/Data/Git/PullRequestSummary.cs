9688a6f2ddbf3e8a46eb6c6af318c4c05b50f4b7
Rob King <rob@gcsuk.co.uk>
2022-11-18 00:49:16 +0000

    Fixed date rendering on pull requests

--- Client/LCARS/Data/Git/PullRequestSummary.cs
+++ Client/LCARS/Data/Git/PullRequestSummary.cs
@@ -13,8 +13,8 @@ public record PullRequestSummary
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? State { get; set; }
-        public string? CreatedOn { get; set; }
-        public string? UpdatedOn { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
         public string? Author { get; set; }
         public int CommentCount { get; set; }
     }
